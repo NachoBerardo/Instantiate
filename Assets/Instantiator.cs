ae4091c062745e411e32f4754aaa0214bd4efdf4
NachoBerardo <nachoberardo2005@gmail.com>
2022-05-09 11:59:28 -0300

    Initial commit

---
+++ Assets/Instantiator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour {
+
+    public GameObject ObjectToClone;
+    public int repeticiones;
+
+    public void Clonar()
+    {
+        for(int i=1; i<=repeticiones; i++ )
+        {
+            Instantiate(ObjectToClone);
+            Debug.Log("Hola");
+        }
+    }
+}
---
+++ Assets/Show_Message_InConsole.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Show_Message_InConsole : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Show_Message()
+    {
+        Debug.Log("Hola");
+    }
+}
